c2949c29597983545c914a5f784cc05584dd2e4e
user <schwenkmark2007@gmail.com>
2023-03-07 00:08:57 +0600

    Добавьте файлы проекта.

---
+++ Работа с указателями.cpp
@@ -0,0 +1,26 @@
+﻿#include <ctime>
+#include <iostream>
+double sizefoo(int *sizearr);
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int play = 1;
+	while (play == 1)
+	{
+		int* sizearr;
+		cout << "введите размер массива" << endl;
+		cin >> (* sizearr);
+		sizefoo(sizearr);
+	}
+}
+double sizefoo(int* sizearr)
+{
+	double* arr = new double[( * sizearr)];
+	return arr[0];
+}
+double arrfoo(one)
+{
+
+}
